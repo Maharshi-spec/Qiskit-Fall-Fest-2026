8a5fbe6eb3e6ed05330ec012664625478f98074e
Tekupudi Balaji <tekupudibalaji@outlook.com>
2026-08-31 11:43:39 +0530

    reset

--- frontend/src/data/event.js
+++
@@ -1,6 +0,0 @@
-export const eventData = {
-  name: 'Qiskit Fall Fest 2026',
-  date: '2026-09-15',
-  location: 'Virtual',
-  description: 'Annual quantum computing event celebrating quantum innovation'
-}
